dd93d74b53d2b06b42530d2c323e21342a27bb5c
Graham Lancashire <F83A537F-79CE508A-B96B5FDE-C7510D25-66E46418>
2025-08-14 11:01:46 +0200

    Version fix

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "VXWalkthroughViewController-Swift",
-	platforms: [
-		.iOS(.v15),
-		.macCatalyst(.v14),
-		],
+    platforms: [
+        .iOS(.v16),
+        .macCatalyst(.v14),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -16,19 +16,20 @@ let package = Package(
             targets: ["VXWalkthrough"]
         ),
     ],
-	dependencies: [
-		.package(url: "https://github.com/swiftmanagementag/QRCodeReader.swift", branch: "master")
-	],
-	targets: [
+    dependencies: [
+        .package(url: "https://github.com/swiftmanagementag/QRCodeReader.swift", branch: "master"),
+    ],
+    targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "VXWalkthrough",
-			dependencies: [
-				.product(name: "QRCodeReader", package: "QRCodeReader.swift")
-			],
-			resources: [
-				.process("Resources"),
-			]),
-	]
+            dependencies: [
+                .product(name: "QRCodeReader", package: "QRCodeReader.swift"),
+            ],
+            resources: [
+                .process("Resources"),
+            ]
+        ),
+    ]
 )
